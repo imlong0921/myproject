403b84351e1460c59f58883247856a3ae9018595
imlong0921 <92108168+imlong0921@users.noreply.github.com>
2021-10-07 23:11:38 +0700

    Add files via upload

---
+++ chuyende1.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+bai1
+	
+int main(int argc, char *argv[]) {
+	printf("==== MOI BAN CHON THUC DON ====");
+	printf("\n\t1: Mon trang mieng");
+	printf("\n\t2: Mon vit nuong");
+	printf("\n\t3: Mon ga luoc");
+	printf("\n\t4: Mon lac rang");
+	
+};
+
+bai2
+
+int main(int argc, char *argv[]) {
+	printf("*************");
+	printf("\n*           *");
+	printf("\n*           *");
+	printf("\n*           *");
+	printf("\n*           *");
+	printf("\n*           *");
+	printf("\n*************");
+}
+
+bai 3
+
+int main(int argc, char *argv[]) {
+	int a,b,c,d,e;
+	printf("Nhap cac so a,b,c,d,e: \n");scanf("%d\n%d\n%d\n%d\n%d",&a,&b,&c,&d,&e);
+	printf("cac so vua nhap: \na = %d\n b = %d\n c = %d\n d = %d\n e = %d\n",a,b,c,d,e);
+}
+
+
+bai 4
+
+int main(int argc, char *argv[]) {
+	int d,m,y;
+	printf("nhap ngay: \n");scanf("%d",&d);
+	printf("nhap thang: \n");scanf("%d",&m);
+	printf("nhap nam: \n");scanf("%d",&y);
+	printf("Ngay thang vua nhap la: %d/%d/%d (Ngay %d thang %d nam %d)",d,m,y,d,m,y);
+}
+
+bai5
+
+int main(int argc, char *argv[]) {
+	int d,m,y;
+	printf("nhap ngay: \n");scanf("%d",&d);
+	printf("nhap thang: \n");scanf("%d",&m);
+	printf("nhap nam: \n");scanf("%d",&y);
+	printf("Ngay thang vua nhap la: %d/%d/%d, nam nay toi %d tuoi",d,m,y,2021-y);
+}
+
+
+bai6
+
+int main(int argc, char *argv[]) {
+	int a,b,c;
+	printf("Nhap vao so co 2 chu so: ");scanf("%d",&a);
+	b=a/10;
+	c=a%10;
+	printf("So vua nhap la: %d => %d chuc %d don vi",a,b,c);
+}
+
+
